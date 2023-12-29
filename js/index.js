98bf2d16b9f9b06756696c5cf74fa8698f82df33
priscilabgmoya <105519676+priscilabgmoya@users.noreply.github.com>
2023-12-29 15:42:53 -0300

    se agrega navbar y footer en index

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 import {mueblesOficina } from '../db/db.js'; 
+import { Footer } from './footer.js';
+import { NavBar } from './navBar.js';
 
 console.log(mueblesOficina);
+
+window.onload= function(){
+    NavBar(); 
+    Footer(); 
+}
