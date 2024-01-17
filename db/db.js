cbaef8fa279f38c22b356b260923f56ae45df6c3
priscilabgmoya <105519676+priscilabgmoya@users.noreply.github.com>
2024-01-17 18:20:50 -0300

    se agrega modificaciones de db

--- db/db.js
+++ db/db.js
@@ -2,6 +2,7 @@ const mueblesOficina = [
     {
       id: 1,
       nombre: "Silla Ergonómica",
+      categoria:"SILLA",
       descripcion: "Silla de oficina ergonómica con respaldo ajustable, diseñada para proporcionar comodidad durante largas horas de trabajo. Tapizada en cuero sintético de alta calidad. Disponible en colores negro y gris.",
       precio: 150,
       descuento: true,
@@ -12,6 +13,7 @@ const mueblesOficina = [
     {
       id: 2,
       nombre: "Escritorio Ejecutivo",
+      categoria:"MESA",
       descripcion: "Escritorio de oficina con diseño ejecutivo, equipado con cajones para un almacenamiento conveniente. Construido con materiales de alta calidad para mayor durabilidad y estilo contemporáneo.",
       precio: 300,
       descuento: true,
@@ -22,6 +24,7 @@ const mueblesOficina = [
     {
       id: 3,
       nombre: "Lámpara LED",
+      categoria:"DECORACION",
       descripcion: "Lámpara de escritorio LED moderna y eficiente energéticamente. Equipada con control táctil para ajustar la intensidad de la luz. Ideal para proporcionar iluminación focal durante tareas detalladas.",
       precio: 50,
       descuento: true,
@@ -32,6 +35,7 @@ const mueblesOficina = [
     {
       id: 4,
       nombre: "Estantería Modular",
+      categoria:"MUEBLE",
       descripcion: "Estantería modular diseñada para adaptarse a diversas necesidades de almacenamiento en la oficina. Estantes ajustables para acomodar diferentes tamaños de objetos. Construcción resistente y acabado contemporáneo.",
       precio: 200,
       descuento: true,
@@ -42,6 +46,7 @@ const mueblesOficina = [
     {
       id: 5,
       nombre: "Archivador Metálico",
+      categoria:"DECORACION",
       descripcion: "Archivador metálico duradero con tres cajones y sistema de bloqueo para mayor seguridad. Ideal para organizar documentos y suministros de oficina. Construcción resistente y diseño funcional.",
       precio: 180,
       descuento: true,
@@ -52,6 +57,7 @@ const mueblesOficina = [
     {
       id: 6,
       nombre: "Mesa de Reuniones",
+      categoria:"MESA",
       descripcion: "Mesa de reuniones espaciosa diseñada para acomodar hasta 6 personas. Superficie resistente y duradera para reuniones productivas. Elegante y funcional, perfecta para espacios de colaboración.",
       precio: 500,
       descuento: true,
@@ -62,6 +68,7 @@ const mueblesOficina = [
     {
       id: 7,
       nombre: "Sillón Ejecutivo",
+      categoria:"SOFA",
       descripcion: "Sillón ejecutivo de oficina con diseño elegante y respaldo alto para un soporte óptimo. Tapizado en cuero genuino para un aspecto lujoso. Base resistente y ruedas giratorias para mayor movilidad.",
       precio: 250,
       descuento: true,
@@ -72,6 +79,7 @@ const mueblesOficina = [
     {
       id: 8,
       nombre: "Armario con Puertas Correderas",
+      categoria:"DECORACION",
       descripcion: "Armario de oficina con puertas correderas para un acceso fácil y ahorro de espacio. Interior configurable para adaptarse a diferentes necesidades de almacenamiento. Construcción robusta y diseño moderno.",
       precio: 400,
       descuento: true,
@@ -82,6 +90,7 @@ const mueblesOficina = [
     {
       id: 9,
       nombre: "Pizarra Magnética",
+      categoria:"DECORACION",
       descripcion: "Pizarra magnética diseñada para presentaciones creativas y organización visual. Superficie magnética para la fijación de notas y documentos. Marco de aluminio resistente y accesorios de montaje incluidos.",
       precio: 80,
       descuento: true,
@@ -92,6 +101,7 @@ const mueblesOficina = [
     {
       id: 10,
       nombre: "Mesa Auxiliar con Ruedas",
+      categoria:"MESA",
       descripcion: "Mesa auxiliar con ruedas para facilitar la movilidad en entornos de oficina flexibles. Superficie resistente y diseño moderno. Ideal para colaboración y áreas de descanso.",
       precio: 120,
       descuento: true,
@@ -102,6 +112,7 @@ const mueblesOficina = [
     {
       id: 11,
       nombre: "Reposapiés Ajustable",
+      categoria:"VARIOS",
       descripcion: "Reposapiés ajustable diseñado para proporcionar comodidad ergonómica durante largas horas de trabajo. Superficie acolchada y ajuste personalizable. Mejora la postura y reduce la fatiga.",
       precio: 30,
       descuento: true,
@@ -112,6 +123,7 @@ const mueblesOficina = [
     {
       id: 12,
       nombre: "Cajonera Móvil con Llave",
+      categoria:"MUEBLE",
       descripcion: "Cajonera móvil con tres cajones y cerradura para mantener documentos y objetos seguros. Ruedas para facilitar la movilidad. Construcción duradera y diseño funcional.",
       precio: 90,
       descuento: true,
@@ -122,6 +134,7 @@ const mueblesOficina = [
     {
       id: 13,
       nombre: "Biblioteca de Pared",
+      categoria:"MUEBLE",
       descripcion: "Biblioteca de pared diseñada para exhibir libros y elementos decorativos. Estantes ajustables para adaptarse a diferentes alturas. Construcción resistente y acabado contemporáneo.",
       precio: 150,
       descuento: true,
@@ -132,6 +145,7 @@ const mueblesOficina = [
     {
       id: 14,
       nombre: "Taburete Alto",
+      categoria:"SILLA",
       descripcion: "Taburete alto con diseño moderno, ideal para zonas de trabajo o barras de cocina. Asiento cómodo y base resistente. Disponible en varios colores para combinar con la decoración.",
       precio: 60,
       descuento: true,
@@ -142,6 +156,7 @@ const mueblesOficina = [
     {
       id: 15,
       nombre: "Candelabro Moderno",
+      categoria:"DECORACION",
       descripcion: "Candelabro de diseño elegante para iluminación ambiental en espacios de oficina. Fabricado con materiales de alta calidad y acabado moderno. Añade un toque de sofisticación al entorno.",
       precio: 40,
       descuento:  true,
@@ -152,6 +167,7 @@ const mueblesOficina = [
     {
       id: 16,
       nombre: "Biombos Separadores",
+      categoria:"DECORACION",
       descripcion: "Biombos separadores diseñados para crear áreas privadas en oficinas abiertas. Estructura resistente y paneles de tela para mayor privacidad. Configuración modular para adaptarse a diferentes espacios.",
       precio: 180,
       descuento: true,
@@ -162,6 +178,7 @@ const mueblesOficina = [
     {
       id: 17,
       nombre: "Organizador de Escritorio",
+      categoria:"MUEBLE",
       descripcion: "Organizador de escritorio con compartimentos para mantener ordenados bolígrafos, clips y otros suministros. Diseño compacto y duradero. Aumenta la eficiencia en el espacio de trabajo.",
       precio: 25,
       descuento: true,
@@ -172,6 +189,7 @@ const mueblesOficina = [
     {
       id: 18,
       nombre: "Papelera de Acero Inoxidable",
+      categoria:"DECORACION",
       descripcion: "Papelera de oficina fabricada en acero inoxidable con diseño moderno y elegante. Tamaño compacto para espacios de trabajo reducidos. Duradera y fácil de limpiar.",
       precio: 20,
       descuento: true,
@@ -182,6 +200,7 @@ const mueblesOficina = [
     {
       id: 19,
       nombre: "Reloj de Pared Moderno",
+      categoria:"DECORACION",
       descripcion: "Reloj de pared moderno diseñado para agregar un toque contemporáneo a oficinas y salas de espera. Fabricado con materiales de alta calidad y mecanismo preciso. Fácil de instalar.",
       precio: 35,
       descuento: true,
@@ -192,6 +211,7 @@ const mueblesOficina = [
     {
       id: 20,
       nombre: "Sofá Modular",
+      categoria:"SOFA",
       descripcion: "Sofá modular diseñado para áreas de descanso y colaboración en entornos de oficina. Asientos cómodos y configuración versátil. Tapizado en tela de alta calidad y disponible en varios colores.",
       precio: 700,
       descuento: true,
